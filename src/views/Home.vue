afdf2ea6357ff745e5f91808483b4fca22bea6bc
Ashes Dangol <ashesdangol@hotmail.com>
2023-07-09 20:55:20 +1000

    fetch data and handle error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" v-if="showPosts"/>
-    <button @click="showPosts = !showPosts">toggle post</button>
-    <button @click="posts.pop()"> delete a post</button>
+    
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>
+      Loading..
+    </div>
+    <div v-if="error">{{error}}</div>
+  
   </div>
 </template>
 
 <script>
-import {ref, computed} from 'vue'
+import {ref} from 'vue'
 import PostList from '../components/PostList.vue'
 
 
@@ -16,20 +22,25 @@ export default {
   name: 'Home',
   components:{PostList},
   setup(){
-   const posts = ref([
-                      {
-                        title:"Welcome to the blof Welcome to the blof Welcome to the blof Welcome to the blof Welcome to the blof",
-                        body:"loreIpsum",
-                        id:1
-                      },
-                      {
-                        title:"Top 5 tips for vue js",
-                          body:"this is another tips for vue.js lore ipsim op 5 tips for vue jsop 5 tips for vue jsop 5 tips for vue jsop 5 tips for vue jsop 5 tips for vue js",
-                          id:2
-                      }
-                    ])
-   const showPosts = ref(true)
-   return {posts,showPosts}
+   const posts = ref([])
+   const error = ref(null)
+  
+  const load = async () => {
+    try {
+      let data = await fetch('http://localhost:3000/posts')
+      console.log(data)
+      if(!data.ok){
+        throw Error('No data Available')
+      }
+      posts.value = await data.json()
+
+    } catch (err) {
+      error.value = err.message
+      console.log(error.value)
+    }
+  }
+  load()
+   return {posts, error}
   },
   
 }
